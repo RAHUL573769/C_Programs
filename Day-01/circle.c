6e603075d7c3876138bb68b88be3a4543288debd
Rahul Rudra <rahulrudra665@gmail.com>
2025-04-27 17:51:52 +0600

    first commit

---
+++ Day-01/circle.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+
+int a,b;
+printf("Enter two numbers");
+scanf("%d %d ",&a,&b);
+printf("The value os a is %d \n",a);
+
+printf("The value os a is %d \n",b);
+
+int sum;
+sum=a+b;
+
+printf("The addidtion value is %d",sum);
+return 0;
+}
