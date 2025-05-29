ff7ca46ba74cf43fea341910b28beef6d1f6ea07
Rahul Rudra <rahulrudra665@gmail.com>
2025-05-29 18:39:16 +0600

    changes commit

---
+++ Day-02/IntroductionToProgrammingLanguage/Week-03/Module-08/inputoutput.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main(){
+int array[5];
+
+int i;
+
+for(i=0;i<=5;i++){
+
+scanf("%d",&array[i]);
+
+}
+
+for (i=0;i<=5;i++)
+{
+
+printf("The i-th element is=%d \n",array[i]);
+
+}
+
+}
