0b6180ae4d05e2f13aacba101aaec80a74558bc5
Rahul Rudra <rahulrudra665@gmail.com>
2025-07-21 14:09:15 +0600

    changes done

---
+++ Day-03/first.py
@@ -0,0 +1,16 @@
+print("Kopa")
+
+a="kota"
+
+print(type(a))
+
+
+name="Rahul Rudra"
+
+age=28
+
+place="Chittagong"
+
+text=f"{name} whose age is {age} lives in {place}"
+
+print(text)
---
+++ Day-03/input.py
@@ -0,0 +1,5 @@
+print("Now I need Money")
+
+money=input("Give me some Money")
+
+print("Here is your Money",money)
